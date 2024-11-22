10d211c5ea4e01ee41b8abb425145ab01485b578
georgolden <georgoldenb@gmail.com>
2024-11-22 20:04:19 +0000

    Fix bugs

--- src/bot.ts
+++ src/bot.ts
@@ -5,6 +5,7 @@ import { handleMessage, handleOutEvent } from './domain/messageHandler';
 import { config } from './config';
 import { ChatMessage } from './domain/types';
 import { SERVICE_NAME, SUMMARY_STREAM } from './domain/constants';
+import { createClient } from 'redis';
 
 // Error handling
 process.on('unhandledRejection', (error: Error) => {
@@ -19,11 +20,14 @@ process.on('uncaughtException', (error: Error) => {
 const init = async () => {
   const whatsappClient = new WhatsAppClient();
   const eventStore = new RedisEventStore(SERVICE_NAME, config.REDIS_URL);
+  const cache = createClient({ url: config.REDIS_URL });
   const dependencies = {
     youtubeRepository: new YoutubeRequestRepository(config.DATABASE_URL),
     eventStore,
     client: whatsappClient,
+    cache,
   };
+  await cache.connect();
   eventStore.processEvents(SUMMARY_STREAM, (event) => handleOutEvent(dependencies, event));
   whatsappClient.on('request', (req: ChatMessage) => handleMessage(dependencies, req));
   whatsappClient.start();
